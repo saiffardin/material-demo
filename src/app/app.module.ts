51bd516dd3ac1d1fc392c19c5916eb52e03ae40e
saiffardin <saiffardin@gmail.com>
2022-04-07 12:09:38 +0600

    nested tree mat UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
@@ -6,7 +8,13 @@ import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatTreeModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
