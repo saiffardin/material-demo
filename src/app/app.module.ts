f0bdbc6c37bbe83c49d5462ad527c9643aee1f31
saiffardin <saiffardin@gmail.com>
2022-04-07 15:00:04 +0600

    split view added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTreeModule } from '@angular/material/tree';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularSplitModule } from 'angular-split';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { NestedTreeComponent } from './nested-tree/nested-tree.component';
+import { SplitViewComponent } from './split-view/split-view.component';
 
 @NgModule({
-  declarations: [AppComponent, NestedTreeComponent],
+  declarations: [AppComponent, NestedTreeComponent, SplitViewComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     MatTreeModule,
     MatIconModule,
+    AngularSplitModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
